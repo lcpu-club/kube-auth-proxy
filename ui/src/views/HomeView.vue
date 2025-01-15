f35c0be03b1011617978c135c6f629074145b380
djdjz7 <djdjz7@icloud.com>
2025-01-15 14:06:14 +0800

    ui: implement ui

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { client } from '@/api/api';
 import { getToken, hasToken } from '@/api/token';
 import { RouterLink } from 'vue-router';
+import { ArrowLeftStartOnRectangleIcon, KeyIcon } from '@heroicons/vue/24/outline';
 
 if (!getToken()) {
   window.location.href = "../oauth/redirect";
@@ -12,16 +13,77 @@ const userInfo = await (await client.get('/_/whoami')).json();
 
 <template>
   <main>
-    <h1>Home</h1>
-    <ul>
-      <li>
-        <RouterLink :to="{ 'name': 'token' }">Token Management</RouterLink>
-      </li>
-      <li>
-        <RouterLink :to="{ 'name': 'logout' }">Logout</RouterLink>
-      </li>
-    </ul>
-    <p>Welcome to the home page!</p>
-    <pre>{{ userInfo }}</pre>
+    <h1 class="page-title">主页</h1>
+    <h2 class="m-0">{{ userInfo.extra.name }}</h2>
+    <div class="flex gap-2 text-gray text-sm">
+      <span>{{ userInfo.extra.realname }}</span>
+      <span>{{ userInfo.extra.school }}</span>
+      <span>{{ userInfo.extra.studentGrade }}</span>
+    </div>
+    <div class="flex col m-t-4 gap-2">
+      <RouterLink :to="{ 'name': 'token' }" class="link">
+        <KeyIcon class="icon" />
+        <span>凭据管理</span>
+      </RouterLink>
+      <RouterLink :to="{ 'name': 'logout' }" class="link">
+        <ArrowLeftStartOnRectangleIcon class="icon" />
+        <span>退出登录</span>
+      </RouterLink>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.m-0 {
+  margin: 0;
+}
+
+.flex {
+  display: flex;
+}
+
+.gap-2 {
+  gap: 0.5rem;
+}
+
+.text-gray {
+  color: #666;
+}
+
+.text-sm {
+  font-size: 0.875rem;
+}
+
+main {
+  padding: 3rem;
+}
+
+.col {
+  flex-direction: column;
+}
+
+.m-t-4 {
+  margin-top: 1rem;
+}
+
+.icon {
+  width: 1em;
+  height: 1em;
+  margin-right: .3em;
+}
+
+.link {
+  color: unset;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  border-bottom: #666 solid 1px;
+  width: fit-content;
+}
+
+@media screen and (max-width: 768px) {
+  main {
+    padding: 1rem;
+  }
+}
+</style>
--- ui/src/views/TokenView.vue
+++ ui/src/views/TokenView.vue
@@ -2,6 +2,7 @@
 import { client } from '@/api/api';
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import { ChevronLeftIcon, EyeIcon, TrashIcon, PlusIcon, EyeSlashIcon } from '@heroicons/vue/24/outline';
 
 const tokens = ref((await (await client.get('/_/tokens')).json()).tokens as string[]);
 
@@ -27,30 +28,152 @@ const show_kubeconfig = async (token: string) => {
 
 <template>
   <main>
-    <h1>Tokens</h1>
-    <p>
-      <RouterLink :to="{ 'name': 'home' }">Back Home</RouterLink>
-    </p>
-    <p>Here are your tokens:</p>
-    <ul>
-      <li v-for="token in tokens" :key="token">
-        <pre>{{ token }}</pre>
-        <ul>
-          <li><button @click="delete_token(token)">Delete</button></li>
-          <li>
-            <button @click="show_kubeconfig(token)">Show Kubeconfig</button>
-          </li>
-        </ul>
-      </li>
-    </ul>
-    <p>
-      <button @click="add_token()">Add Token</button>
-    </p>
-    <div v-if="kubeconfig">
-      Kubeconfig for
-      <pre style="display: inline;">{{ kubeconfig_for }}</pre> <br />
-      <button @click="kubeconfig = '';">Hide</button> <br />
-      <textarea rows="16" cols="80" disabled>{{ kubeconfig }}</textarea>
+    <h1 class="flex items-center">
+      <RouterLink :to="{ 'name': 'home' }" class="title-back">
+        <ChevronLeftIcon class="icon" />
+      </RouterLink>
+      <span class="flex-grow">凭据列表</span>
+      <button @click="add_token()" class="icon-button">
+        <PlusIcon class="icon" />
+        <span>添加凭据</span>
+      </button>
+    </h1>
+    <div class="token-wrapper" v-for="token in tokens" :key="token">
+      <pre class="token-cell m-0">{{ token }}</pre>
+      <button @click="show_kubeconfig(token)" class="icon-button">
+        <EyeIcon class="icon" />
+        <span>显示 Kubeconfig</span>
+      </button>
+      <button @click="delete_token(token)" class="icon-button">
+        <TrashIcon class="icon" />
+        <span>删除</span>
+      </button>
+    </div>
+    <div v-if="kubeconfig" class="m-t-2">
+      <div class="flex gap-2 items-center text-no-wrap">
+        <span class="font-bold">Kubeconfig for</span>
+        <pre class="inline flex-grow flex-shrink overflow-auto font-bold">{{ kubeconfig_for }}</pre>
+        <button @click="kubeconfig = '';" class="icon-button">
+          <EyeSlashIcon class="icon" />
+          <span>隐藏</span>
+        </button>
+      </div>
+      <textarea class="m-t-2" rows="16" cols="80" disabled>{{ kubeconfig }}</textarea>
     </div>
   </main>
 </template>
+
+<style scoped>
+.m-0 {
+  margin: 0;
+}
+
+.flex {
+  display: flex;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.gap-2 {
+  gap: 0.5rem;
+}
+
+main {
+  padding: 3rem;
+  box-sizing: border-box;
+}
+
+.col {
+  flex-direction: column;
+}
+
+.m-t-2 {
+  margin-top: 0.5rem;
+}
+
+.icon {
+  width: 1em;
+  height: 1em;
+  margin-right: .3em;
+}
+
+.title-back {
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  color: unset;
+}
+
+@media screen and (max-width: 768px) {
+  main {
+    padding: 1rem;
+  }
+}
+
+button {
+  padding: 0.4em 0.8em;
+  border: none;
+  background-color: transparent;
+  border-radius: 0.3em;
+  width: max-content;
+  transition: background-color 0.1s;
+}
+
+button:hover {
+  background-color: rgba(100, 100, 100, 0.1);
+}
+
+.icon-button {
+  display: flex;
+  align-items: center;
+  padding: 1em;
+  text-wrap-mode: nowrap;
+}
+
+.token-cell {
+  overflow: auto;
+  text-overflow: unset;
+  text-wrap-mode: nowrap;
+  flex-grow: 1;
+  padding: 1em 1em;
+}
+
+.token-wrapper {
+  display: flex;
+  align-items: stretch;
+}
+
+.token-wrapper:nth-child(even) {
+  background-color: rgba(200, 200, 200, 0.1);
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.flex-shrink {
+  flex-shrink: 1;
+}
+
+.inline {
+  display: inline;
+}
+
+.text-no-wrap {
+  text-wrap-mode: nowrap;
+}
+
+.overflow-auto {
+  overflow: auto;
+}
+
+.font-bold {
+  font-weight: bold;
+}
+
+textarea {
+  max-width: 100%;
+}
+</style>
